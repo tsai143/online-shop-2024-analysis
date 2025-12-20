9c19deb3e0518efd416cc7f457c65a7999e21e0d
tsai143 <tsaikumarsai143@gmail.com>
2025-12-20 21:21:49 +0530

    Week 1: minor update to database creation script

--- sql/01_create_database.sql
+++ sql/01_create_database.sql
@@ -1,3 +1,19 @@
 CREATE DATABASE IF NOT EXISTS online_shop_2024;
 USE online_shop_2024;
 
+SHOW DATABASES;
+SHOW TABLES;
+SELECT COUNT(*) FROM customers;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS payment;
+SELECT COUNT(*) FROM customers;
+SELECT COUNT(*) FROM orders;
+SELECT COUNT(*) FROM order_items;
+SELECT COUNT(*) FROM products;
+SELECT COUNT(*) FROM payment;
+SELECT COUNT(*) FROM reviews;
+SELECT COUNT(*) FROM shipments;
+SELECT COUNT(*) FROM suppliers;
+
+
+
